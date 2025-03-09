7d1d52c20ba9ff858ad84947619cacf1981d5d27
Andrew Smith <meuzishun@gmail.com>
2025-03-09 15:06:11 -0400

    chore: Include CompanyLogo in Header (AP-169)

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,13 @@
 'use client';
+import CompanyLogo from '@/app/components/CompanyLogo';
 import Navbar from '@/app/components/Navbar';
-import { LogoutBtn } from './LogoutBtn';
+import LogoutBtn from '@/app/components/LogoutBtn';
 
 export default function Header() {
   return (
     <header className='bg-white text-black p-4 shadow-md'>
       <div className='container mx-auto flex justify-between items-center'>
-        {/* Replace h1 with svg image later */}
-        <h1 className='text-xl font-semibold'>Tenant</h1>
+        <CompanyLogo />
         <Navbar />
         {/* notification icon dealio */}
         <LogoutBtn />
