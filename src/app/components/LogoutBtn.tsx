02e5b8bff91f810444eab1fa04fb7513d2a7c44d
Andrew Smith <meuzishun@gmail.com>
2025-03-10 17:36:37 -0400

    fix: Use utility color in LogoutBtn (AP-169)

--- src/app/components/LogoutBtn.tsx
+++ src/app/components/LogoutBtn.tsx
@@ -6,7 +6,7 @@ export default function LogoutBtn() {
 
   return (
     <button
-      className='flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-lg font-medium md:flex-none md:justify-start md:p-2 md:px-8 text-white bg-secondary-button-blue'
+      className='flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-lg font-medium md:flex-none md:justify-start md:p-2 md:px-8 text-white bg-secondary-blue'
       onClick={handleLogoutClick}
     >
       Logout
