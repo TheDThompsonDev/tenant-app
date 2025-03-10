2c2595284661d543dc2d4405384f280d73664438
Andrew Smith <meuzishun@gmail.com>
2025-03-10 10:31:27 -0400

    chore: Adjust styles for LogoutBtn with hard-coded colors (AP-169)

--- src/app/components/LogoutBtn.tsx
+++ src/app/components/LogoutBtn.tsx
@@ -6,7 +6,7 @@ export default function LogoutBtn() {
 
   return (
     <button
-      className='flex h-[48px] grow items-center justify-center gap-2 rounded-md hover:bg-gray-50 p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3'
+      className='flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-lg font-medium md:flex-none md:justify-start md:p-2 md:px-8 text-white bg-[#164E63]'
       onClick={handleLogoutClick}
     >
       Logout
