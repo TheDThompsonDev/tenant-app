cd6a4c14447e5c7dd1495a4bce1b6030a6f952e2
casacava <cassiopeiacavazos@gmail.com>
2025-03-10 16:17:00 -0500

    chore: created/landingpage for hero content- AP-141

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Header from '@/app/components/Header';
-import Hero from "./components/Hero"
 
 export default function Home() {
   return (
     <div>
       <Header />
       Home Page
-      <Hero />
     </div>
-  );
+  )
 }
