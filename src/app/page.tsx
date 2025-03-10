4f55dc2a6cc9b7e1aef84707de950ef69341d2b3
casacava <cassiopeiacavazos@gmail.com>
2025-03-10 14:14:31 -0500

    feat: implemented hero image + get started button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from '@/app/components/Header';
+import Hero from "./components/Hero"
 
 export default function Home() {
   return (
     <div>
       <Header />
       Home Page
+      <Hero />
     </div>
   );
 }
