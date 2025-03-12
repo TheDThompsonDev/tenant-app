14419a8c63e1d84e6e3c4d01130a4da3419f3cd5
Andrew Smith <meuzishun@gmail.com>
2025-03-12 18:41:04 -0400

    feat: add navigation labels (AP-194)

--- src/app/constants/labels.ts
+++ src/app/constants/labels.ts
@@ -1,16 +1,37 @@
 const LABELS = {
-    contactUs: {
-        title: "Contact Us",
-        description:
-          "Looking for a new place to call home? Contact us today to learn more about our available apartments!",
-          form: {
-            fullName: "Full Name",
-            phone: "Phone",
-            email: "Email",
-          },
-        formSubmitButton: "Send",
-      },
+  navigation: {
+    messages: {
+      href: '/',
+      text: 'Messages',
+      icon: 'messageIcon',
+    },
+    unlock_door: {
+      href: '/',
+      text: 'Unlock My Door',
+      icon: 'lockIcon',
+    },
+    packages: {
+      href: '/',
+      text: 'My Packages',
+      icon: 'packageIcon',
+    },
+    parking: {
+      href: '/',
+      text: 'Guest Parking Pass',
+      icon: 'parkingIcon',
+    },
+  },
+  contactUs: {
+    title: 'Contact Us',
+    description:
+      'Looking for a new place to call home? Contact us today to learn more about our available apartments!',
+    form: {
+      fullName: 'Full Name',
+      phone: 'Phone',
+      email: 'Email',
+    },
+    formSubmitButton: 'Send',
+  },
 };
 
 export default LABELS;
-
