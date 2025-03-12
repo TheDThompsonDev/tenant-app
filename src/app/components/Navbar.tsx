78f824df8f49fae38b68ed4f114ae216c04f1ad8
Andrew Smith <meuzishun@gmail.com>
2025-03-12 09:58:50 -0400

    Create NotificationBadge and add it to Navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link';
+import NotificationBadge from '@/app/components/NotificationBadge';
 
 export default function Navbar() {
+  const mockNotificationCount = 4; // TODO: replace this with data from server (hook?)
+
   const linkClasses =
     'flex h-[48px] grow items-center justify-center gap-2 rounded-md hover:bg-gray-50 p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3';
 
@@ -9,6 +12,7 @@ export default function Navbar() {
       <ul className='flex space-x-4'>
         <Link href='/' className={linkClasses}>
           <p className='hidden md:block text-lg'>Messages</p>
+          <NotificationBadge value={mockNotificationCount} />
         </Link>
         <Link href='/' className={linkClasses}>
           <p className='hidden md:block text-lg'>Unlock Door</p>
---
+++ src/app/components/NotificationBadge.tsx
@@ -0,0 +1,24 @@
+interface NotificationBadgeProps {
+  value: number;
+  className?: string;
+}
+
+export default function NotificationBadge({
+  value,
+  className,
+}: NotificationBadgeProps) {
+  if (value <= 0) {
+    return null;
+  }
+
+  const displayValue = value > 99 ? '99+' : value;
+  const textSize = value > 99 ? 'text-[9px]' : 'text-xs';
+
+  return (
+    <div
+      className={`w-6 h-6 bg-red-500 text-white flex items-center justify-center rounded-full font-bold ${textSize} ${className}`}
+    >
+      {displayValue}
+    </div>
+  );
+}
