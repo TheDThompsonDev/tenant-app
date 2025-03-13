1fe8e050aadf25e038f353b0e82cd23ea78d2b22
casacava <cassiopeiacavazos@gmail.com>
2025-03-12 19:06:21 -0500

    fix: added damiens PR into landingpage (AP-183)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 // should be what people see when they first access our website
 import Hero from "./components/Hero"
 import HeroPool from "./components/HeroPool"
+import ContactUs from "./components/ContactUs"
 
 export default function LandingPage() {
   return (
     <main>
       <Hero />
-      <HeroPool /> 
+      <HeroPool />
+      <ContactUs />
     </main>
   )
 }
