b40947f356ff7d755526d0bf28e9bbcb1e5fbfab
Damian Padilla <parallaxfilmsdfw@gmail.com>
2025-03-13 03:48:17 -0500

    feat: created feature highlight section and cards. (AP-177)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,16 @@ import Hero from "./components/Hero"
 import HeroPool from "./components/HeroPool"
 import ContactUs from "./components/ContactUs"
 import Header from "./components/Header"
+import FeatureHighlight from "./components/FeatureHighlight"
 
 export default function LandingPage() {
   return (
     <main>
       <Header />
       <Hero />
+      <FeatureHighlight />
       <HeroPool />
       <ContactUs />
     </main>
-  )
+  );
 }
