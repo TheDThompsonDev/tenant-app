8978383377d3e08ca73abd6c8920015980f776ab
Cass C <105602784+casacava@users.noreply.github.com>
2025-03-12 19:17:50 -0500

    Moved <ContactUs> Component into landing page (AP-183) (#18)
    * fix: added damiens PR into landingpage (AP-183)
    
    * fix: added in Andrew's header PR into landing page (AP-183)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 // should be what people see when they first access our website
 import Hero from "./components/Hero"
 import HeroPool from "./components/HeroPool"
+import ContactUs from "./components/ContactUs"
+import Header from "./components/Header"
 
 export default function LandingPage() {
   return (
     <main>
+      <Header />
       <Hero />
-      <HeroPool /> 
+      <HeroPool />
+      <ContactUs />
     </main>
   )
 }
