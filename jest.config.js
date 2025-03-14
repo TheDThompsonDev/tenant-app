f199f1b4e347057c23d6db6b7a822bdc0aa2ac75
Danny <dannythompson901@gmail.com>
2025-03-14 17:49:27 -0500

    chore: remove comments (AP-199)

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,10 @@
-// @ts-check
-
 import nextJest from 'next/jest.js';
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 });
 
-// Add any custom config to be passed to Jest
+
 /** @type {import('jest').Config} */
 const config = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
@@ -17,5 +14,4 @@ const config = {
   },
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config);
