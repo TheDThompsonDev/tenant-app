ba23351042baf85dc0effc161195409e1bf30e94
meuzishun <76052265+meuzishun@users.noreply.github.com>
2025-03-13 23:58:52 -0400

    fix: refactor Header and Navbar so logic is only in two files (AP-194) (#17)
    * feat: add navigation labels (AP-194)
    
    * feat: create icon map (AP-194)
    
    * chore: map over navigation labels in Navbar (AP-194)
    
    * chore: refactor MobileNav (AP-194)
    
    Break up into smaller components, use labelMap and iconMap, and create a dir for subcomponents and index file for clean imports
    
    * fix: refactor Header and Navbar so logic is in only two files (AP-194)
    
    * fix: remove unnecessary component files (AP-194)

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,34 +3,35 @@ import { useState } from 'react';
 import CompanyLogo from '@/app/components/CompanyLogo';
 import Navbar from '@/app/components/Navbar';
 import LogoutBtn from '@/app/components/LogoutBtn';
-import { Menu } from 'lucide-react';
-import MobileNav from '@/app/components/MobileNav';
 import Image from 'next/image';
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const openMenu = () => setIsMenuOpen(true);
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
   const closeMenu = () => setIsMenuOpen(false);
 
   const profileImage = '/Animal.jpg'; // TODO: Replace with the actual profile image source
+  const userName = 'Animal';
+  const userEmail = 'wildanimal@email.com';
 
   return (
     <header className='relative text-white bg-[url("/street-view.jpeg")] bg-cover bg-center lg:bg-none lg:bg-white lg:text-black py-4 px-6'>
-      {/* Mobile */}
       <div className='absolute inset-0 bg-black/20 lg:hidden' />
-      <div className='relative z-10 flex flex-col'>
-        <div className='flex justify-between items-center lg:hidden'>
+
+      {/* Mobile View */}
+      <div className='relative z-10 lg:hidden'>
+        <div className='flex justify-between items-center'>
           <CompanyLogo />
-          <button
-            className='lg:hidden p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400'
-            onClick={openMenu}
-          >
-            <Menu size={36} />
-          </button>
-          {isMenuOpen && <MobileNav closeMenu={closeMenu} />}
+          <Navbar
+            isMobile={true}
+            isMenuOpen={isMenuOpen}
+            toggleMenu={toggleMenu}
+            closeMenu={closeMenu}
+          />
         </div>
-        <div className='mt-14 flex flex-col items-center lg:hidden'>
+
+        <div className='mt-14 flex flex-col items-center'>
           <Image
             src={profileImage}
             width={24}
@@ -38,15 +39,15 @@ export default function Header() {
             alt='Profile Picture'
             className='w-24 h-24 rounded-full object-cover ring-4 ring-white'
           />
-          <h3 className='mt-6 text-3xl'>Animal</h3>
-          <p>wildanimal@email.com</p>
+          <h3 className='mt-6 text-3xl'>{userName}</h3>
+          <p>{userEmail}</p>
         </div>
       </div>
 
-      {/* Desktop */}
+      {/* Desktop View */}
       <div className='hidden mx-auto lg:flex justify-between items-center'>
         <CompanyLogo />
-        <Navbar />
+        <Navbar isMobile={false} />
         <div className='flex flex-row gap-4 items-center justify-center'>
           <Image
             src={profileImage}
--- src/app/constants/labels.ts
+++ src/app/constants/labels.ts
@@ -1,52 +1,79 @@
 const LABELS = {
-    featureHighlight: {
-      title: "Modern Problems Require Moderns Solutions",
-      featureCards: [
-        {
-          id:"feature-001",      
-          title: "AI Escalation",
-          description: "Our AI system detects repeated complaints and automatically escalates unresolved issues."
-        },
-        {
-          id:"feature-002",    
-          title:"Smart Lock ",
-          description:"Unlock doors and grant guest access with a tap. No keys needed.",      
-        },
-        {
-          id:"feature-003",    
-          title:"Secure Package Locker ",
-          description:"Receive packages securely with smart locker pickup codes.",      
-        },
-        {
-          id:"feature-004",    
-          title:"Guest Parking Permits",
-          description:"Generate digital parking passes for guests in seconds.",      
-        },
-        {
-          id:"feature-005",    
-          title:"One-Tap Noise Reporting",
-          description:"Report noise or disturbances instantly for a quick resolution.",      
-        },
-        {
-          id:"feature-006",    
-          title:"Digital Lease Signing",
-          description:"Sign and renew leases digitally - fast, secure, and hassle-free.",      
-        },
-      ],
+  navigation: {
+    messages: {
+      href: '/',
+      text: 'Messages',
+      icon: 'messageIcon',
     },
+    unlock_door: {
+      href: '/',
+      text: 'Unlock My Door',
+      icon: 'lockIcon',
+    },
+    packages: {
+      href: '/',
+      text: 'My Packages',
+      icon: 'packageIcon',
+    },
+    parking: {
+      href: '/',
+      text: 'Guest Parking Pass',
+      icon: 'parkingIcon',
+    },
+  },
 
-    contactUs: {
-        title: "Contact Us",
+  featureHighlight: {
+    title: 'Modern Problems Require Moderns Solutions',
+    featureCards: [
+      {
+        id: 'feature-001',
+        title: 'AI Escalation',
         description:
-          "Looking for a new place to call home? Contact us today to learn more about our available apartments!",
-          form: {
-            fullName: "Full Name",
-            phone: "Phone",
-            email: "Email",
-          },
-        formSubmitButton: "Send",
+          'Our AI system detects repeated complaints and automatically escalates unresolved issues.',
       },
+      {
+        id: 'feature-002',
+        title: 'Smart Lock ',
+        description:
+          'Unlock doors and grant guest access with a tap. No keys needed.',
+      },
+      {
+        id: 'feature-003',
+        title: 'Secure Package Locker ',
+        description:
+          'Receive packages securely with smart locker pickup codes.',
+      },
+      {
+        id: 'feature-004',
+        title: 'Guest Parking Permits',
+        description: 'Generate digital parking passes for guests in seconds.',
+      },
+      {
+        id: 'feature-005',
+        title: 'One-Tap Noise Reporting',
+        description:
+          'Report noise or disturbances instantly for a quick resolution.',
+      },
+      {
+        id: 'feature-006',
+        title: 'Digital Lease Signing',
+        description:
+          'Sign and renew leases digitally - fast, secure, and hassle-free.',
+      },
+    ],
+  },
+
+  contactUs: {
+    title: 'Contact Us',
+    description:
+      'Looking for a new place to call home? Contact us today to learn more about our available apartments!',
+    form: {
+      fullName: 'Full Name',
+      phone: 'Phone',
+      email: 'Email',
+    },
+    formSubmitButton: 'Send',
+  },
 };
 
 export default LABELS;
-
