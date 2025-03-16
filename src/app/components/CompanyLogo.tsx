726cfff55b9c2d698c1192de62029649573d4d90
meuzishun <76052265+meuzishun@users.noreply.github.com>
2025-03-16 19:02:50 -0400

    chore: add a little margin to the bottom of the svg in CompanyLogo so it lines up with the text (TP-37) (#43)

--- src/app/components/CompanyLogo.tsx
+++ src/app/components/CompanyLogo.tsx
@@ -5,7 +5,7 @@ export default function CompanyLogo() {
         xmlns='http://www.w3.org/2000/svg'
         viewBox='0 0 24 24'
         fill='currentColor'
-        className='w-8 h-8 text-black'
+        className='w-8 h-8 text-black mb-1'
       >
         <path d='M3 22V7.5L12 2l9 5.5V22h-7v-6h-4v6H3ZM5 20h4v-4h6v4h4V8.68l-7-4.28-7 4.28V20ZM7 10h2v2H7v-2Zm0 4h2v2H7v-2Zm4-4h2v2h-2v-2Zm0 4h2v2h-2v-2Zm4-4h2v2h-2v-2Zm0 4h2v2h-2v-2Z' />
       </svg>
