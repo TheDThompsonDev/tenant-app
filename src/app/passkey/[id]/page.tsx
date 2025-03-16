fb5e4a8d1b35c294720290942d3ac53d530ea9fe
Joe Aguado <aguado.joe@gmail.com>
2025-03-15 21:12:07 -0500

    feat: update index mapping and passcode UI (AP-201)

--- src/app/passkey/[id]/page.tsx
+++ src/app/passkey/[id]/page.tsx
@@ -1,69 +1,78 @@
-"use client"
+"use client";
 import { useSearchParams } from "next/navigation";
 import { Send } from "lucide-react";
-
-
+import Header from "@/app/components/Header";
 
 interface Params {
-    id: string;
+  id: string;
 }
 
-interface KeyPageProps{
-    params: Promise<Params>
+interface KeyPageProps {
+  params: Promise<Params>;
 }
 
 export default function KeyPage({ params }: KeyPageProps) {
   const searchParams = useSearchParams();
-  
-  if(!searchParams){
-    return <div>Loading...</div>
+
+  if (!searchParams) {
+    return <div>Loading...</div>;
   }
 
-  const userName = searchParams.get("userName")
-  const date = searchParams.get("date")
-  const status = searchParams.get("status")
-  const code = searchParams.get("code")
-  const splitCode = code ? code.split("") : []
+  const tenant = searchParams.get("tenant");
+  const unit = searchParams.get("unit");
+  const date = searchParams.get("date");
+  const status = searchParams.get("status");
+  const code = searchParams.get("code");
+  const splitCode = code ? code.split("") : [];
 
   const emailBody = `Hello, here is your guest key code: ${code}`;
-  const mailtoLink = `mailto:?subject=Guest Key Code&body=${encodeURIComponent(emailBody)}`;
+  const mailtoLink = `mailto:?subject=Guest Key Code&body=${encodeURIComponent(
+    emailBody
+  )}`;
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-white">
-      <h1 className="text-2xl font-bold mb-4">New Key Details</h1>
-      <div className="bg-alternate-green rounded-lg shadow-md text-black p-4">
-        <span className="flex">
-        {userName}
-<p className="pl-1">created a new guest key code!
-</p>
-        </span>
-        {/*strong>Key ID: {params.id}</p> */}
-        <p>Date: {date}</p>
-        <p>Status: {status}</p>
+    <>
+      <Header />
+      <div className="flex flex-col items-center pt-10 min-h-screen p-4 bg-white text-center">
+        <h1 className="text-2xl font-bold mb-4 text-primary-black">
+          New Key Details
+        </h1>
+        <div className="bg-alternate-green rounded-lg shadow-md text-black p-4">
+          <span className="flex pb-4 text-lg">
+            {tenant}
+            <p className="pl-1">created a new guest key code!</p>
+          </span>
+          {/*strong>Key ID: {params.id}</p> */}
+          <p>Date: {date}</p>
+          <p>Status: {status}</p>
 
+          <div className="flex flex-col justify-center items-center">
+            <p className="text-xl font-bold pt-4">Unit # {unit}</p>
 
-    <div className="flex justify-center items-center">
-
-        <div className="flex my-4 gap-2 ">
-            {splitCode.map((digit, index) =>(
-                <div key={index}
-                className="flex border-1 border-black p-4 rounded-md bg-alternate-light-gray shadow-lg">
+            <div className="flex items-center mt-2">
+              <div className="flex gap-2">
+                {splitCode.map((digit, index) => (
+                  <div
+                    key={`${digit}-${index}`}
+                    className="flex border-1 border-black p-4 rounded-md bg-alternate-light-gray shadow-lg"
+                  >
                     <p className="text-2xl font-bold">{digit}</p>
-                    </div>
-            ))}
-        </div>
-            <a href={mailtoLink}>
-                <Send className="text-black w-8 h-8 ml-4"/>
-            </a>
-                
+                  </div>
+                ))}
+              </div>
+              <a href={mailtoLink}>
+                <Send className="text-black w-6 h-6 ml-4" />
+              </a>
             </div>
+          </div>
+        </div>
+        <a
+          href="/passkey"
+          className="mt-4 px-6 py-2 bg-secondary-blue text-white rounded-lg font-bold"
+        >
+          Make another key
+        </a>
       </div>
-      <a
-        href="/passkey"
-        className="mt-4 px-6 py-2 bg-secondary-blue text-white rounded-lg font-bold"
-      >
-       Make another key
-      </a>
-    </div>
+    </>
   );
 }
--- src/app/passkey/page.tsx
+++ src/app/passkey/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useRouter } from "next/navigation";
+import Header from "@/app/components/Header";
 import Lock from "../components/Lock";
 import { v4 as uuidv4 } from "uuid";
 
@@ -15,30 +16,34 @@ export default function Home() {
     const keyData = {
       id: uuidv4(),
       code: generatePasscode(),
-      userName: "Tenant 01",
+      tenant: "Tenant 01",
+      unit: "202",
       date: new Date().toLocaleDateString(),
       status: "Active",
     };
-
+    console.log(keyData);
     router.push(
-        `/passkey/${keyData.id}?code=${keyData.code}&userName=${keyData.userName}&date=${keyData.date}&status=${keyData.status}`
-      );
-    };
+      `/passkey/${keyData.id}?code=${keyData.code}&tenant=${keyData.tenant}&unit=${keyData.unit}&date=${keyData.date}&status=${keyData.status}`
+    );
+  };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-white">
-      <div className="flex flex-col items-center text-center py-8">
-        <h1 className="text-2xl font-bold pb-1">Door Lock</h1>
-        <h2 className="text-xl">Unit #2</h2>
-      </div>
-      <Lock initialLockedState={false} />
+    <>
+      <Header />
+      <div className="flex flex-col items-center min-h-screen pt-2 p-4 bg-white">
+        <div className="flex flex-col items-center text-center py-8 text-primary-black">
+          <h1 className="text-2xl font-bold pb-1">Door Lock</h1>
+          <h2 className="text-xl">Unit #2</h2>
+        </div>
+        <Lock initialLockedState={false} />
 
-      <button
-        onClick={handleGenerateKey}
-        className="px-10 py-3 bg-button text-white text-2xl font-bold rounded-lg mt-10 bg-secondary-blue hover:translate-y-0.5 hover:cursor-pointer"
-      >
-        Generate Key
-      </button>
-    </div>
+        <button
+          onClick={handleGenerateKey}
+          className="px-10 py-3 bg-button text-white text-2xl font-bold rounded-lg mt-10 bg-secondary-blue hover:translate-y-0.5 hover:cursor-pointer"
+        >
+          Generate Key
+        </button>
+      </div>
+    </>
   );
 }
