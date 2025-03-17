6300c13420343a9c10fbfb449800481debc5c50b
DavieDevs <199727366+DavieDevs@users.noreply.github.com>
2025-03-17 15:14:48 -0500

    Ap 203 fix (#46)
    * feat: implemented package list, package details, locker details (Ap-203)
    
    * chore: moved dummy package data to api (AP-203)
    
    * Fix: copied and added packages api from previous commit. (AP-203)

--- src/app/locker/page.tsx
+++ src/app/locker/page.tsx
@@ -5,6 +5,8 @@ import LABELS from "../constants/labels";
 import Header from "../components/Header";
 import PackageCard from "../components/PackageCard";
 
+import { packages } from "@/app/api/packages/route";
+
 export default function Home() {
   const router = useRouter();
 
@@ -13,12 +15,12 @@ export default function Home() {
       <Header />
       <div className="bg-white min-h-screen ">
         <h2 className="text-black text-3xl text-center font-bold p-4">
-          My Packages
+          {LABELS.packageList.title}
         </h2>
 
         {/* TODO: Fetch package list from the database instead of using hardcoded data*/}
         <div className=" flex items-center flex-col justify-center m-4">
-          {LABELS.package.packageCards.map((item) => {
+          {packages.map((item) => {
             return (
               <div
                 className="w-full md:w-1/2 m-4"
