014b967ef3e291dee2e2fa94cc4a217268473c4a
chanpi23 <channycodez@gmail.com>
2025-03-19 22:38:02 -0600

    TP-33

---
+++ pages/api/lease/[id].ts
@@ -0,0 +1,44 @@
+import { prisma } from "../../../utils/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      try {
+        const { id } = req.query;
+        const lease = await prisma.lease.findUnique({
+          where: { id: id as string },
+        });
+        res.status(200).json(lease);
+      } catch (error) {
+        console.error("Error finding lease", error);
+        res.status(500).json({ error: "failed to fetch lease" });
+      }
+      break;
+
+    case "DELETE":
+      try {
+        const { id } = req.query;
+        if (!id) {
+          return res.status(400).json({ error: "Contact ID is required" });
+        }
+        const lease = await prisma.lease.delete({
+          where: { id: id as string },
+        });
+        res.status(200).json(lease);
+      } catch (error) {
+        console.error("Error deleting lease:", error);
+        res.status(500).json({ error: "Failed to delete lease" });
+      }
+      break;
+
+    default:
+      res.setHeader("Allow", ["GET", "DELETE"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
---
+++ pages/api/lease/index.ts
@@ -0,0 +1,59 @@
+import { prisma } from "../../../utils/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      try {
+        const lease = await prisma.lease.findMany();
+        res.status(200).json(lease);
+      } catch (error) {
+        console.error("Error finding lease:", error);
+        res.status(500).json({ error: "failed to fetch lease" });
+      }
+      break;
+
+  //case "POST":
+    try {
+        const { fullName, email, phoneNumber, subject, message } = req.body;
+
+      const requiredFields = ["fullName", "phoneNumber", "email"];
+       const missingFields = requiredFields.filter(
+      (field) => !req.body[field]
+      );
+
+        if (missingFields.length > 0) {
+          return res
+            .status(400)
+            .json({ error: `Missing fields: ${missingFields.join(", ")}` });
+        }
+
+        const contactUs = await prisma.contactUs.create({
+          data: {
+            fullName,
+            email,
+            phoneNumber,
+            subject,
+            message,
+            createdAt: new Date(),
+          },
+        });
+        res.status(201).json(contactUs);
+      } catch (error) {
+        console.error("Error creating contact info:", error);
+        res.status(500).json({
+          error: "failed to create contact info",
+        });
+      }
+      break;
+
+    default:
+      res.setHeader("Allow", ["GET", "POST"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
