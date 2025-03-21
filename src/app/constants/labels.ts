ba8144cbfdfd871f436c7cc750eff121a2420260
Joe Aguado <aguado.joe@gmail.com>
2025-03-21 08:21:06 -0500

    chore: changed admin message routes back to /Messaging, deleted working directories (TP-57)

--- src/app/constants/labels.ts
+++ src/app/constants/labels.ts
@@ -160,7 +160,7 @@ const LABELS = {
   },
   adminDashboardBtns: {
     messages: {
-      href: "/adminMessage",
+      href: "/Messaging",
       text: "View Messages",
       icon: "mailIcon",
     },
