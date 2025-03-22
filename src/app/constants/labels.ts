79b5b72740335060bd7626b491523754187082ac
Andrew Smith <meuzishun@gmail.com>
2025-03-22 11:37:52 -0400

    feat: add change password form and btn labels (TP-66)

--- src/app/constants/labels.ts
+++ src/app/constants/labels.ts
@@ -220,6 +220,17 @@ const LABELS = {
     },
   },
 
+  changePassword: {
+    formLabels: {
+      currPassword: 'Current Password',
+      newPassword: 'New Password',
+    },
+    formBtns: {
+      cancelBtn: 'Cancel',
+      submitBtn: 'Submit',
+    }
+  },
+
   passcode: {
     searchParams: "Loading...",
     title: "New Key Details",
