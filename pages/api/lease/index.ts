70a2b068680c1f4fe3aa69aafb59c852a2eefdfe
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2025-03-23 11:11:32 -0500

    updated notification schema to make sender optional so that contact us can send notifications to admin

--- pages/api/lease/index.ts
+++ pages/api/lease/index.ts
@@ -17,8 +17,6 @@ export default async function handler(
         res.status(500).json({ error: "failed to fetch users" });
       }
       break;
-
-    case "POST":
       try {
         const {
           appwriteId,
@@ -67,7 +65,7 @@ export default async function handler(
       break;
 
     default:
-      res.setHeader("Allow", ["GET", "POST"]);
+      res.setHeader("Allow", ["GET"]);
       res.status(405).end(`Method ${method} Not Allowed`);
   }
 }
