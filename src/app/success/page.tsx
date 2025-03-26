1fa6b74d2010ba1333c298c94628f323132c4fbd
Staci Southerland <stacisoutherland24@gmail.com>
2025-03-26 17:37:53 -0500

    chore:add styling to success screen to match other screens TP-125 (#122)

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { CircleUser} from "lucide-react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import LABELS from "../constants/labels";
@@ -25,29 +26,55 @@ function SuccessContent() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4 relative">
+    <div className="min-h-screen flex flex-col justify-center bg-gray-100 p-4 relative">
       <Link
         href="/createTenant"
-        className="text-med text-gray-500 mb-4 inline-block absolute top-10 left-10"
-      >
+        className="text-med text-gray-500 mb-4 inline-block absolute top-10 left-10">
         &larr; {LABELS.success.back}
       </Link>
+      <div className="max-w-3xl mx-auto bg-white rounded-xl shadow-lg overflow-hidden">
+        <div className="bg-gradient-to-r from-primary-green to-secondary-blue p-7 text-white">
+          <h2 className="text-2xl font-bold flex items-center">
+            <CircleUser className="mr-3 w-10 h-10 md:w-7 md:h-7" />
+            {LABELS.success.title}
+          </h2>
+        </div>
+        <div className="grid grid-cols-12">
+          <div className="col-span-7 border-r border-gray-200 p-6">
+            <h3 className="text-lg underline font-semibold mb-4 text-gray-500">
+              {LABELS.success.credentialsMessage}
+            </h3>
+            <p className="mb-10 mt-5">
+              <span
+                className="font-semibold text-gray-500
+              ">
+                {LABELS.success.email}
+              </span>{" "}
+              <br />
+              <span
+                className="font-semibold text-gray-500
+              ">
+                {email}
+              </span>
+            </p>
+            <p>
+              <span className="font-semibold  text-gray-500">
+                {LABELS.success.temporaryPassword}
+              </span>{" "}
+              <br />
+              <span
+                className="font-semibold  text-gray-500">
+                {password}
+              </span>
+            </p>
+          </div>
 
-      <h2 className="text-xl font-semibold text-gray-800 text-center">
-        {LABELS.success.title}
-      </h2>
-      <div className="mt-6 bg-secondary-blue text-white rounded-xl p-6 shadow-lg h-56 w-72 sm:w-80 md:w-96 lg:w-[30rem]">
-        <p className="mb-10 mt-5 ml-5">
-          <span className="font-semibold underline">Email</span> <br />
-          {email}
-        </p>
-        <p className="ml-5">
-          <span className="font-semibold underline">
-            {LABELS.success.temporaryPassword}
-          </span>{" "}
-          <br />
-          {password}
-        </p>
+          <div className="col-span-5 p-5 flex items-center justify-center">
+            <p className="text-gray-500 text-lg">
+              {LABELS.success.registrationMessage}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
