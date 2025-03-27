7998ccc6dca642a256fb1f1b89da37fe0b4e4534
Joe <139164599+joeoverflowcode@users.noreply.github.com>
2025-03-26 23:43:24 -0500

    chore: updated styling on door lock feature and guest passkey (T\P-133) (#129)
    Co-authored-by: Danny <dannythompson901@gmail.com>

--- src/app/components/Lock.tsx
+++ src/app/components/Lock.tsx
@@ -1,33 +1,36 @@
 import { useState } from "react";
 import { LockKeyhole, LockKeyholeOpen } from "lucide-react";
 
-
 interface LockProps {
-    initialLockedState: boolean;
+  initialLockedState: boolean;
 }
-const Lock = ({initialLockedState}: LockProps) => {
+const Lock = ({ initialLockedState }: LockProps) => {
   const [isLocked, setIsLocked] = useState(initialLockedState);
 
   return (
-    <button
-      className={`w-80 h-20 flex items-center rounded-2xl p-10 transition duration-300   ${
-        isLocked ? "bg-alternate-grey" : "bg-primary-green"
-      }`}
-      onClick={() => setIsLocked(!isLocked)}
-    >
-      <div
-        className={`w-80 h-20 flex items-center justify-center bg-alternate-light-gray rounded-2xl shadow-md transform transition duration-300 ${
-          isLocked ? "-translate-x-10" : "translate-x-10"
+    <div>
+      <button
+        className={`w-80 h-20 flex items-center rounded-2xl p-10 transition duration-300   ${
+          isLocked ? "bg-alternate-grey" : "bg-primary-green"
         }`}
+        onClick={() => setIsLocked(!isLocked)}
       >
-        {isLocked ? (
-          <LockKeyholeOpen className="text-alternate-gray" />
-        ) : (
-          <LockKeyhole className="text-green-600" />
-        )}
+        <div
+          className={`w-80 h-20 flex items-center justify-center bg-alternate-light-gray rounded-2xl shadow-md transform transition duration-300 ${
+            isLocked ? "-translate-x-10" : "translate-x-10"
+          }`}
+        >
+          {isLocked ? (
+            <LockKeyholeOpen className="text-alternate-gray" />
+          ) : (
+            <LockKeyhole className="text-green-600" />
+          )}
+        </div>
+      </button>
+      <div className="text-xl font-bold mt-8">
+        {isLocked ? "Unlocked" : "Locked!"}
       </div>
-
-    </button>
+    </div>
   );
 };
 
