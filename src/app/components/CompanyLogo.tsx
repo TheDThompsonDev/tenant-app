2fd653afd9d973bdca87255e489b96ce2a4f9317
Joe <139164599+joeoverflowcode@users.noreply.github.com>
2025-03-28 16:40:30 -0500

    chore: add color prop to logo in footer (#132)

--- src/app/components/CompanyLogo.tsx
+++ src/app/components/CompanyLogo.tsx
@@ -10,11 +10,11 @@ export default function CompanyLogo({ isFooter = false }) {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
-          className="w-8 h-8 text-primary-black"
+          className={`w-8 h-8 ${isFooter}`}
         >
           <path d="M3 22V7.5L12 2l9 5.5V22h-7v-6h-4v6H3ZM5 20h4v-4h6v4h4V8.68l-7-4.28-7 4.28V20ZM7 10h2v2H7v-2Zm0 4h2v2H7v-2Zm4-4h2v2h-2v-2Zm0 4h2v2h-2v-2Zm4-4h2v2h-2v-2Zm0 4h2v2h-2v-2Z" />
         </svg>
-        <h1 className="text-2xl text-primary-black mt-2 font-semibold">TENANT</h1>
+        <h1 className={`text-2xl  mt-2 font-semibold ${isFooter}`}>TENANT</h1>
       </Link>
     </div>
   );
